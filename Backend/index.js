d8556314619dfcf46f6585f2114257a2191a9ecb
Jaya <jayarohith162@gmail.com>
2024-06-15 10:30:04 -0500

    create routing file structure

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
+const mainRouter = require("./routes/index");
 
+app.use(cors());
 app.use(express.json());
-app.use(3000);
+app.use("/api/v1", mainRouter);
 
 app.listen(3000);
