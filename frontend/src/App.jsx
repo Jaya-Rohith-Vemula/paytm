69718d1a42d28715c0d8bdada883d2387422a072
Jaya <jayarohith162@gmail.com>
2024-06-22 20:18:01 -0500

    create routes with react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" />
+          <Route path="/signin" />
+          <Route path="/dashboard" />
+          <Route path="/send" />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
